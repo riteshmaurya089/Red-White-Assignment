2cf180bb8ffc793866cd159b9517da26485c3858
RITESH <ayanmaurya89@gmail.com>
2024-04-30 15:11:49 +0530

    project complete

--- Frontend/app/src/App.js
+++ Frontend/app/src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// src/App.js
+import React from 'react';
+import {  Routes, Route } from 'react-router-dom';
+import ProductList from './components/ProductList';
+import ProductDetail from './components/ProductDetail';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <Routes>
+          <Route  path="/" element={<ProductList/>} />
+          <Route  path="/products/:id" element={<ProductDetail/>} />
+          <Route path="/cart" element={<Cart/>} />
+          <Route path="/checkout" element={<Checkout />} /> 
+          </Routes>
+      </div>
+      
   );
-}
+};
 
 export default App;
